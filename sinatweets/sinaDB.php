8aa29781c6d72a360baa2971baba916dc656025e
chenkehxx <chenkehxx@126.com>
2014-03-25 00:56:09 +0800

    add something

--- sinatweets/sinaDB.php
+++ sinatweets/sinaDB.php
@@ -6,13 +6,15 @@
     mysql_query("set names gb2312");
 
     /*table tweets*/
-    $sql = "CREATE TABLE tweets(
-            name varchar(20) NOT NULL,
-            tweetid varchar(20) NOT NULL,
+    $sql = "CREATE TABLE newtweets(
+            tweetid varchar(20) NOT NULL UNIQUE,    /*微博id*/
+            name varchar(100) NOT NULL,         /*用户名*/
             userid varchar(20) NOT NULL,
-            source varchar(100) NOT NULL,
+            source varchar(100) NOT NULL,       /*微博来源*/
             tweettext varchar(280) NOT NULL,
-            create_at varchar(80) NOT NULL,
+            create_at varchar(80) NOT NULL,         /*微博时间*/
+            reposts varchar(6) NOT NULL,            /*微博转发数*/
+            comments varchar(6) NOT NULL,           /*微博评论数*/
             PRIMARY KEY (tweetid)
         )";
     mysql_query($sql, $con);
--- sinatweets/store.php
+++ sinatweets/store.php
@@ -7,8 +7,10 @@
    $text = isset($_POST['text']) ? $_POST['text'] : '';
    $name = isset($_POST['username']) ? $_POST['username'] : '';
    $userid = isset($_POST['userid']) ? $_POST['userid'] : '';
+   $resposts = isset($_POST['resposts']) ? $_POST['resposts'] : '';
+   $comments = isset($_POST['comments']) ? $_POST['comments'] : '';
 
-   $sql = "insert into tweets (name, tweetid, userid, source, tweettext, create_at) values('$name', '$tweetid', '$userid', '$source', '$text', '$create_at')";
+   $sql = "insert into tweets (name, tweetid, userid, source, tweettext, create_at, resposts, comments) values('$name', '$tweetid', '$userid', '$source', '$text', '$create_at', '$resposts', '$comments')";
    $res = mysql_query($sql, $con);
    // echo $sql."\n";
    if ($res) {
