986c9aeaab4fbdb7d8eb02a9e10e7ae2b9f7c130
chenkehxx <chenkehxx@126.com>
2014-03-25 21:04:21 +0800

    finish all thing

--- sinatweets/storeDB.php
+++ sinatweets/storeDB.php
@@ -2,25 +2,30 @@
   require('config.php');
 
    $data = isset($_POST['data']) ? $_POST['data'] : '';
-   var_dump($data);
+   // var_dump($data);
    $num = count($data);
-   for ($i = 0; $i < $num; $i += 8) {
-        $name = $data[$i];
-        $tweetid = $data[$i+1];
-        $userid = $data[$i+2];
-        $source = $data[$i+3];
-        $text = $data[$i+4];
-        $create_at = $data[$i+5];
-        $reposts = $data[$i+6];
-        $comments = $data[$i+7];
-        $sql = "insert into newtweets (tweetid, name, userid, source, tweettext, create_at, reposts, comments) values('$tweetid', '$name', '$userid', '$source', '$text', '$create_at', '$reposts', '$comments')";
-        $res = mysql_query($sql, $con);
-       // echo $sql."\n";
-       if ($res) {
-            echo 'insert success!'."\n";
-       } else {
-            echo 'error!'."\n";
+   if ($data) {
+        for ($i = 0; $i < $num; $i += 8) {
+            $name = $data[$i];
+            $tweetid = $data[$i+1];
+            $userid = $data[$i+2];
+            $source = $data[$i+3];
+            $text = $data[$i+4];
+            $create_at = $data[$i+5];
+            $reposts = $data[$i+6];
+            $comments = $data[$i+7];
+            $sql = "insert into newtweets (tweetid, name, userid, source, tweettext, create_at, reposts, comments) values('$tweetid', '$name', '$userid', '$source', '$text', '$create_at', '$reposts', '$comments')";
+            $res = mysql_query($sql, $con);
+           // echo $sql."\n";
+           if ($res) {
+                // echo 'insert success!'."\n";
+           } else {
+                // echo 'error!'."\n";
+           }
        }
+   } else {
+        echo "no data!";
    }
+   
    
   ?>
